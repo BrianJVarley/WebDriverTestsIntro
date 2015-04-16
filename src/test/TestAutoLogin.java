2f145ef824c1285490d46d6118892bd3bc6afcd1
BrianJVarley <brianvarley9@gmail.com>
2015-04-16 12:45:31 +0100

    initial commit - April 16/15

---
+++ src/test/TestAutoLogin.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class TestAutoLogin {
+
+	@Test
+	public void testHomePage(){
+	
+	FirefoxDriver driver=new FirefoxDriver();
+	driver.get("http://gmail.com");
+	WebElement username =driver.findElement(By.id("Email"));
+	username.sendKeys("dummyname");
+	
+	WebElement password =driver.findElement(By.name("Passwd"));
+	password.sendKeys("xxxxxx");
+	
+	WebElement button =driver.findElement(By.id("signIn"));
+	button.click();
+
+	}
+	
+
+}
---
+++ src/test/TestingBrowserOpen.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestingBrowserOpen {
+	
+	public static void main(String[] args) {
+		
+		
+		FirefoxDriver driver = new FirefoxDriver();
+		
+	}
+
+}
